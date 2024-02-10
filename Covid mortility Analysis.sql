a62b2c1b9e05bbbb7f8114e33fa7c3537a4d3b2b
Shruti Vats <shrutivats.0503@gmail.com>
2024-02-10 18:01:41 +0530

    Doing my SQL part

--- Covid mortility Analysis.sql
+++ Covid mortility Analysis.sql
@@ -19,3 +19,19 @@ where location= 'India' order by 1,2
 select location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as MortatlityRate
 from Covid19_Mortality_Insight..[Covid Death] 
 where location like '%states%' order by 1,2
+
+--Looking at Total Cases vs Total Deaths
+--what percentage of population got Covid-19
+select location, date, total_cases, population, (total_cases/population)*100 as AffectedPopulationPercentages
+from Covid19_Mortality_Insight..[Covid Death] 
+order by 1,2
+
+select location, date, total_cases, population, (total_cases/population)*100 as AffectedPopulationPercentages
+from Covid19_Mortality_Insight..[Covid Death] 
+where location= 'India' order by 1,2
+
+--Country with Highest Infection Rate
+select location, population , max(total_cases) as HighestInfectionCount , max((total_cases/population))*100 as AffectedPopulationPercentages
+from Covid19_Mortality_Insight..[Covid Death] group by location, population order by AffectedPopulationPercentages desc
+
+--Countries with Highest Motality per Population
